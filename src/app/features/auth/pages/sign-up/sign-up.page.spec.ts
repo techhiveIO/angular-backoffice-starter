f910d50dbf055cf690f75e0718f86e3313fa165d
aliobaji <ali@techhive.io>
2020-06-15 17:05:46 +0300

    feat(sign up): wrote tests

--- src/app/features/auth/pages/sign-up/sign-up.page.spec.ts
+++ src/app/features/auth/pages/sign-up/sign-up.page.spec.ts
@@ -1,14 +1,92 @@
-import {TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {SignupPageComponent} from './sign-up.page';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {BrowserTestingModule} from '@angular/platform-browser/testing';
 
 describe('SignUpPage', () => {
-  let page: SignupPageComponent;
+  let fixture: ComponentFixture<SignupPageComponent>;
+  let component: SignupPageComponent;
+  let mockedFormBuilder: jasmine.SpyObj<FormBuilder>;
+
+  const mockedFormGroup: FormGroup = new FormBuilder().group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
   const configureTestingModule: () => void = () => {
+    mockedFormBuilder = jasmine.createSpyObj('FormBuilder', ['group']);
+    mockedFormBuilder.group.and.returnValue(mockedFormGroup);
+
     TestBed.configureTestingModule({
+      imports: [
+        BrowserTestingModule
+      ],
       declarations: [SignupPageComponent],
+      providers: [
+        {provide: FormBuilder, useValue: mockedFormBuilder},
+      ],
+    }).compileComponents();
+  };
+
+  const initializeTestComponent: () => void = (): void => {
+    fixture = TestBed.createComponent(SignupPageComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  };
+
+  describe('onInit', () => {
+    beforeEach(async(() => {
+      configureTestingModule();
+    }));
+
+    beforeEach(() => {
+      initializeTestComponent();
     });
 
-    page = TestBed.inject(SignupPageComponent);
-  };
+    it('should build the login form', () => {
+      expect(mockedFormBuilder.group).toHaveBeenCalledTimes(1);
+      expect(mockedFormBuilder.group).toHaveBeenCalledWith({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      });
+    });
+  });
+
+  describe('toggleHidePassword', () => {
+    beforeEach(async(() => {
+      configureTestingModule();
+    }));
+
+    beforeEach(() => {
+      initializeTestComponent();
+    });
+
+    it('should invert the hidden flag if clicked', () => {
+      const defaultFlagValue = false;
+      component.hidden = defaultFlagValue;
+      component.toggleHidePassword();
+
+      expect(component.hidden).not.toBe(defaultFlagValue);
+    });
+  });
+
+  describe('OnSubmit', () => {
+    beforeEach(async(() => {
+      configureTestingModule();
+    }));
+
+    beforeEach(() => {
+      initializeTestComponent();
+    });
+
+    it('should set the loading flag', () => {
+      component.onSubmit();
+      expect(component.isLoading).toBe(true);
+    });
+  });
 });
