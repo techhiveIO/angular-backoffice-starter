7e2744fd54da958fa9b87f73aaaae50a0b33d029
aliobaji <ali@techhive.io>
2020-06-18 15:10:33 +0300

    feat(users selector): wrote tests refactor(users actions): removed spec file

--- src/app/features/manage-users/store/users.selectors.spec.ts
+++ src/app/features/manage-users/store/users.selectors.spec.ts
@@ -1,3 +1,18 @@
-describe('UsersSelectors', () => {
+import {User} from '../../../core/models/user.model';
+import {MOCKED_API_USER} from '../../../core/mocks/users.mocks';
+import {selectAllUsers} from './users.selector';
+import {initialState} from './users.reducer';
 
+describe('UsersSelectors', () => {
+  describe('selectAllUsers', () => {
+    it('should return an array of users', () => {
+      const usersArray = [new User(MOCKED_API_USER)];
+      const state = {
+        ...initialState,
+        users: usersArray,
+      };
+
+      expect(selectAllUsers.projector(state)).toEqual(usersArray);
+    });
+  });
 });
